c7b0aa40f03553e3ff1f00392795ef0c36e7bdec
Lili Boxer <ninaliboxer@gmail.com>
2019-04-25 15:29:31 -0700

    TDD working

--- test/tests.js
+++ test/tests.js
@@ -1,13 +1,24 @@
 const test = QUnit.test;
+    
+function feedCataDog(mockCaterpillarDog, donutName) {
+    const span = document.createElement('span');
+    span.classList.add('part');
+    span.classList.add(donutName);
+
+    mockCaterpillarDog.appendChild(span);
+}
 
 test('put donut into caterpillar dog', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-    
+    const donutName = 'blue-donut';
+    const mockCaterpillarDog = document.createElement('div');
+    const expected = '<span class="part blue-donut"></span>';
 
     //Act 
     // Call the function you're testing and set the result to a const
-
+    feedCataDog(mockCaterpillarDog, donutName);
     //Assert
-    assert.equal(true, false);
+    const parentInnerHTML = mockCaterpillarDog.innerHTML;
+    assert.equal(parentInnerHTML, expected);
 });
