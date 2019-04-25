cfc0652d532d3cb6241363f98de85791df6ff39b
Lili Boxer <ninaliboxer@gmail.com>
2019-04-25 14:53:09 -0700

    added event listener and for loop

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-const 
+const donutButtons = document.querySelectorAll('.donut');
+
+for (let i = 0; i < donutButtons.length; i++) {
+    const donutButton = donutButtons[i];
+
+    feedCaterpillarDog.addEventListener('click', () => {
+        
+
+    }); 
+}
